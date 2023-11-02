aa9928833028bd69138033055a0a0f5a388b2996
nurdin <taabaldyev.nurdin27@mail.ru>
2023-11-02 19:28:10 +0600

    final

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,3 +38,7 @@ urlpatterns = [
     path('api/v1/', include('order.urls')),
     path('api/v1/', include('review.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
